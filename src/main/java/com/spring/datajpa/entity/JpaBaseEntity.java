96a6021a26814e91c6fd350564f7a77e22256861
jhchoi <jhchoig9@gamil.com>
2024-10-20 16:03:44 +0900

    Auditing 순수 JPA

---
+++ src/main/java/com/spring/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,28 @@
+package com.spring.datajpa.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+@MappedSuperclass
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+}
